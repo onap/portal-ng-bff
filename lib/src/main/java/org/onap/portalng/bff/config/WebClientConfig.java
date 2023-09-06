c3c2eec967891108459c45786f8e98a85004e678
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2023-09-06 08:32:33 +0200

    Add Micrometer [bff]
    - add Micrometer dependencies
    - uses Zipkin as trace protocol, typically for port 9411
    - requires changing dependency injection of WebClient.Builder [1]
    
    [1] Micrometer is injecting the trace context into the WebClient.Builder bean.
    To add the ExchangeFilterFunctions for authentication, errorhandling and logging, the existing bean needs to be modified instead of statically creating a new one
    
    Issue-ID: PORTALNG-57
    Change-Id: I13dd18d297cd56fa2dfbb525723c79f4abb41f87
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

---
+++ lib/src/main/java/org/onap/portalng/bff/config/WebClientConfig.java
@@ -0,0 +1,56 @@
+package org.onap.portalng.bff.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+  @Component
+  public static class WebClientBeanPostProcessor implements BeanPostProcessor {
+
+    private final ExchangeStrategies exchangeStrategies;
+    private final ExchangeFilterFunction idTokenExchangeFilterFunction;
+    private final ExchangeFilterFunction errorHandlingExchangeFilterFunction;
+    private final ExchangeFilterFunction logResponseExchangeFilterFunction;
+
+    public WebClientBeanPostProcessor(
+        ExchangeStrategies exchangeStrategies,
+        @Qualifier(BeansConfig.ID_TOKEN_EXCHANGE_FILTER_FUNCTION)
+            ExchangeFilterFunction idTokenExchangeFilterFunction,
+        @Qualifier(BeansConfig.ERROR_HANDLING_EXCHANGE_FILTER_FUNCTION)
+            ExchangeFilterFunction errorHandlingExchangeFilterFunction,
+        @Qualifier(BeansConfig.LOG_RESPONSE_EXCHANGE_FILTER_FUNCTION)
+            ExchangeFilterFunction logResponseExchangeFilterFunction) {
+      this.exchangeStrategies = exchangeStrategies;
+      this.idTokenExchangeFilterFunction = idTokenExchangeFilterFunction;
+      this.errorHandlingExchangeFilterFunction = errorHandlingExchangeFilterFunction;
+      this.logResponseExchangeFilterFunction = logResponseExchangeFilterFunction;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+        throws BeansException {
+      return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+        throws BeansException {
+      if (bean instanceof WebClient.Builder builder) {
+        return builder
+            .exchangeStrategies(exchangeStrategies)
+            .filter(idTokenExchangeFilterFunction)
+            .filter(errorHandlingExchangeFilterFunction)
+            .filter(logResponseExchangeFilterFunction);
+      }
+      return bean;
+    }
+  }
+}
